310bc4292085fad7923c493ba4c27acc00150220
Prashant. Nagar <Prashant.Nagar@netprophetsglobal.com>
2025-03-03 18:36:25 +0530

    Router added of extra pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home.js";
 import About from "./pages/about.js";
+import Services from "./pages/services.js";
+import ContactUs from "./pages/contactus.js";
+import News from "./pages/news.js";
+import Page from "./pages/page.js";
 
 export default function App() {
   return (
@@ -9,6 +13,10 @@ export default function App() {
         <Routes>
           <Route path="home" element={<HomePage />} />
           <Route path="about" element={<About />} />
+          <Route path="services" element={<Services />} />
+          <Route path="contact" element={<ContactUs />} />
+          <Route path="news" element={<News />} />
+          <Route path="pages" element={<Page />} />
         </Routes>
       </BrowserRouter>
     </>
